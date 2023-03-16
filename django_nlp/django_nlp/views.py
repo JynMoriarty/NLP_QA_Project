981b7c303eb768d02008822365e0f7101b4be88c
benalieur <benjamin.quinet59b@gmail.com>
2023-03-16 15:58:20 +0100

    feat: :art: Ajout d'un input pdf

--- django_nlp/django_nlp/views.py
+++ django_nlp/django_nlp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
 
-    return render(request, "django_nlp/index.html")
+    if request.method == 'POST':
+        question = request.POST.get('question')
+        pdf = request.POST.get('pdf')
+
+        data = {
+            'question': question,
+            'pdf': pdf,
+        }
+
+        # headers = {"Content-Type": "application/json"}
+
+        # url = "https://url-de-l'api"
+
+        # response = requests.post(url, json=data, headers=headers)
+
+    return render(request, "django_nlp/index.html")#, response)
