7aa62080ba2f38a1065f71a8ee80794bbf6ecf44
benalieur <benjamin.quinet59b@gmail.com>
2023-03-16 16:46:46 +0100

    fix: :bug: Redirection des statics files

--- django_nlp/django_nlp/settings.py
+++ django_nlp/django_nlp/settings.py
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'fr-FR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
@@ -109,6 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
