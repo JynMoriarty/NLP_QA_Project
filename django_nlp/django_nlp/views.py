a85df4778add69e51dc387324c139dce4f4c739d
benalieur <benjamin.quinet59b@gmail.com>
2023-03-17 18:13:59 +0100

    Merge branch 'modelisation' of github.com:JynMoriarty/NLP_QA_Project into develop

--- django_nlp/django_nlp/views.py
+++ django_nlp/django_nlp/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from .forms import UploadFileForm
-import streamlit as st
 import requests
 from PyPDF2 import PdfReader
+from .functions import get_answer
 
 
 @csrf_exempt
@@ -27,10 +27,11 @@ def index(request, **kwargs):
 
             for page in reader.pages:
                 text += page.extract_text()
-
+            result = get_answer(text,question,0.5)
             data = {
                 'question' : question,
                 'texte' : text,
+                'result': result
             }
 
             return render(request, "django_nlp/result.html", data)
