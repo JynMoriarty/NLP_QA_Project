26364305a8cff537a139f7dec743994af78d0b44
benalieur <benjamin.quinet59b@gmail.com>
2023-03-17 18:05:14 +0100

    style: :art: Début du CSS de la page Index.html
    Mise en forme des inputs : Question, PDF

--- django_nlp/django_nlp/forms.py
+++ django_nlp/django_nlp/forms.py
@@ -4,3 +4,11 @@ class UploadFileForm(forms.Form):
     
     question = forms.CharField(max_length=200)
     file = forms.FileField()
+
+    def __init__(self, *args, **kwargs):
+
+        super(UploadFileForm, self).__init__(*args, **kwargs)
+
+        self.fields['question'].widget.attrs.update({'placeholder':"Entrez votre question"})
+
+        self.fields['file'].widget.attrs.update({'class':'pdf'})
