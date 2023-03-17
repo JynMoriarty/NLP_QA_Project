bd0fe7be97a3a73bf3a12117502dfdb58e05c7f3
JynMoriarty <108009430+JynMoriarty@users.noreply.github.com>
2023-03-17 15:56:00 +0100

    feat: Implémentation de l'api ACS dans django

--- django_nlp/django_nlp/functions.py
+++ django_nlp/django_nlp/functions.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 #pip install python-dotenv
 #pip install azure-ai-language-questionanswering
 
-def get_answer(text,question,co):
+def get_answer(text,question,accuracy):
 
     load_dotenv()
     endpoint = "https://question-answering-test-simplon.cognitiveservices.azure.com/"
@@ -21,13 +21,12 @@ def get_answer(text,question,co):
 
         output = client.get_answers_from_text(input)
 
-    best_answer = [a for a in output.answers if a.confidence > 0.5]
+    best_answer = [a for a in output.answers if a.confidence > accuracy]
 
     
 
     dico = {}
-    dico['reponse']= best_answer.answer
+    dico['reponse']= best_answer[0].answer
     dico['score']=output.answers[0].confidence
 
     return dico
-
